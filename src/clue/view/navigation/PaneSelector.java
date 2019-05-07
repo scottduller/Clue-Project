e39ae3925d6dc2d7213315d4577c5c836e946163
Scott <scottduller@gmail.com>
2019-05-07 23:01:37 +0100

    Fail

--- src/clue/view/navigation/PaneSelector.java
+++ src/clue/view/navigation/PaneSelector.java
@@ -1,11 +1,7 @@
 package clue.view.navigation;
 
-public enum PaneSelector {
-    ROOT("clue/view/fxml/navigation/RootLayout.fxml"),
-    MENU("clue/view/fxml/startMenu/MenuLayout.fxml"),
-    GAME("clue/view/fxml/mainBoard/BoardLayout.fxml"),
-    SETTINGS("clue/view/fxml/startMenu/SettingsMenuLayout.fxml"),
-    PLAYER_SELECT("clue/view/fxml/startMenu/PlayerSelectLayout.fxml");
+public class PaneSelector {
+    
 
     private final String PATH;
 
